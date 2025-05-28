d7229887f3ce66655280baa7673f432259867f0b
danielditoo <danieldito2004@students.unnes.ac.id>
2025-05-28 14:08:52 +0700

    Update 1_Dashboard.py

--- pages/1_Dashboard.py
+++ pages/1_Dashboard.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-st.set_page_config(page_tittle="Dashboard", page_icon="")
+st.set_page_config(page_title="Dashboard", page_icon="")
 st.tittle("Tes")
 st.sidebar.header("Dashboard")
